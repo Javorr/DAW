5ab4a23d3a9eaad5c0affcf89769c921d03e7c4b
Javorr <43468316+Javorr@users.noreply.github.com>
2018-10-25 19:45:17 +0200

    rayada del github

--- practica1/controlacceso.php
+++ practica1/controlacceso.php
@@ -7,10 +7,9 @@ $password = $_GET['password'];
 if(($nombre=='usuario1' and $password=='usuario1') or ($nombre=='usuario2' and $password=='usuario2')
     or ($nombre=='usuario3' and $password=='usuario3') or ($nombre=='usuario4' and $password=='usuario4')) header("Location: usuario.php");
 
-else{
-
-header("Location: index.php?error=true");
-}
+    else{
+    header("Location: index.php?error=true");
+    }
 
 
  ?>
