ebacce2f1bac7c66e1185a43de9bac72206c0739
Javorr <43468316+Javorr@users.noreply.github.com>
2018-11-30 20:18:28 +0100

    registro nuevo usuario casi hecho

--- practica1/respregistro.php
+++ practica1/respregistro.php
@@ -12,27 +12,36 @@
     $rnombre=$_POST['nombre'];
     $rpass=$_POST['password'];
     $rpass2=$_POST['password2'];
+    $remail=$_POST['email'];
+    $rsexo=$_POST['genero'];
+    $rfecha=$_POST['fecha'];
+    $rciudad=$_POST['ciudad'];
+    $rpais=$_POST['pais'];
 
     if (!(empty($rnombre) && empty($rpass) && empty($rpass2) )) { //si se ha escrito algo en los campos
       if ($rpass == $rpass2) { //si contraseña y repetir contraseña se repiten entonces se habran realizado las comprobaciones y se mostrara la informacion introducida
 
-        if(preg_match("/[^\x00-\x7F]/", $rnombre)) { //filtro el nombre de usuario
-            if(3<=strlen($rnombre)<=15) {
-              //entonces guay
-            }
-        }
+        if(preg_match('/^[0-9A-Za-z]{3,15}$/', $rnombre)) { //filtro el nombre de usuario
+
+            if(preg_match('/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])[0-9A-Za-z_]{6,15}$/', $rpass)) { //filtro la password
+
+              if(!empty($remail) && filter_var($remail, FILTER_VALIDATE_EMAIL)) { //filtro el email
+
+                if(!empty($rsexo)) { //filtro el sexo
+
+                  if(checkdate($rfecha['month'], $rfecha['day'], $rfecha['year'])) { //filtro la fecha de nacimiento
 
 
-        require("requires/mysqli.php");
+                    require("requires/mysqli.php");
 
-          if ($mysqli->connect_error) {
-              die("Connection failed: " . $mysqli->connect_error);
-          }
-          else {
+                      if ($mysqli->connect_error) {
+                          die("Connection failed: " . $mysqli->connect_error);
+                      }
+                      else {
 
-            $sql = "SELECT * FROM paises order by NomPais";
-            $consulta = $mysqli->query($sql);
-          }
+                        $sql = "INSERT INTO usuarios VALUES ('$rnombre', '$rpass', '$remail', '$rsexo', '$rfecha')";
+                        $consulta = $mysqli->query($sql);
+                      }
 
 
 echo<<<EOF
@@ -50,6 +59,31 @@ echo<<<EOF
                           </ul>
                         </section>
 EOF;
+
+
+                  }
+                  else {
+                    echo "Introduzca una fecha de nacimiento válida. <br><br>";
+                  }
+
+                }
+                else {
+                  echo "Introduzca un sexo válido. <br><br>";
+                }
+
+              }
+              else {
+                echo "Introduzca un email válido. <br><br>";
+              }
+
+            }
+            else {
+              echo "Introduzca una contraseña válida. <br><br>";
+            }
+        }
+        else {
+          echo "Introduzca un nombre de usuario válido. <br><br>";
+        }
 
       }
       else {
